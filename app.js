6bae016ddd60acf81b618d5838fbb1d29404160a
newbaraa1988 <>
2021-06-02 15:33:08 +0000

    world of cake

---
+++ app.js
@@ -0,0 +1,14 @@
+alert(  'hello to my website')
+var name = "world of cake"
+var a = prompt('do you like cake ?)
+  if (a == 'yes') {
+   document.write('<h3>' + userAnswer + '</h3>')
+
+ }
+ else if (a == 'whatever') {
+   document.write('<h3>' + userAnswer + '</h3>')
+
+ }
+
+ else  if{
+   alert('please type the correct type of cake')
