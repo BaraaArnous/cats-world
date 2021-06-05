bfe19bf71ae5b122e992aff97b03e79bb0eabbae
newbaraa1988 <>
2021-06-05 18:53:03 +0000

    cake16

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 alert(  'hello to my website')
 var name = "world of cake"
-var a = prompt('do you like cake ?)
+var a = prompt('do you like cake ?')
   if (a == 'yes') {
-   document.write('<h3>' + userAnswer + '</h3>')
+   document.write('<h3>' + a + '</h3>')
 
  }
- else if (a == 'whatever') {
-   document.write('<h3>' + userAnswer + '</h3>')
+ else if (a == 'no') {
+  document.write('<h3>' + a + '</h3>') 
 
  }
 
- else  if{
-   alert('please type the correct type of cake')
+ else  {
+   alert('please answer yes or no')
+ }
+   var nm =prompt ('how many cake do you want?please less than 3')
+   while (nm>3)
+{
+  nm =prompt ('how many cake do you want?please less than five')
+}
+for(var i=0;i<nm;i++)
+ {
+
+
+ document.write (' <img src="https://www.biggerbolderbaking.com/wp-content/uploads/2021/01/Sour-Cream-Pound-Cake-Thumbnail-scaled.jpg"hight="100px"width="100px"/>')
+ 
+ }
